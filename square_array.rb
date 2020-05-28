701abda1905f229a74daf04d3cd4d47648e61a68
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 15:10:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   numbers = [1,2,3]
   numbers.each do |number|
-    number**2
     puts "The number #{number} squared is #{number**2}."
+    number=number**2
     squared_numbers >> number
   end
 end
