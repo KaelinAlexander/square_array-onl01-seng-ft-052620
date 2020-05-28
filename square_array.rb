11d02fb0bc2f46827370b10f63cbf532b4c5cd9d
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 15:21:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,11 @@
 def square_array(array)
   array=[1, 2, 3]
-  array.each do 
+  array.each do |number|
+    squared_number = number**2
+    if number=1 
+      square_array = []
+    end 
+    square_array >> squared_number
+  return square_array
+  end 
 end
